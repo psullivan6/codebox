f3b7e507f9a08d0f001c8600742f7ba4eb501601
Patrick Sullivan <Sully@Sullys-MBP.local>
2013-06-01 15:44:51 -0700

    real js file

---
+++ codebox.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+	
+	$('.cdbx').each(function(){
+		
+		var blacklist = [
+			'cdbx',
+			'color',
+			'd-in-block'
+		],
+		$this = $(this),
+		cls = $this.attr('class').split(/\s+/),
+		names = '',
+		i;
+		
+		for (i = 0; i < blacklist.length; i++) {
+			
+			if ($.inArray(blacklist[i], cls) > -1){
+				
+				cls.splice($.inArray(blacklist[i], cls), 1);
+				
+			};
+			
+		};
+		
+		$.each(cls, function(k, v){
+			
+			names = (k > 0) ? names + ' .' + v : '.' + v;
+			
+		});
+		
+		$this.prepend('<input class="code" type="text" value="' + names + '" size="' + names.length + '" onclick="this.select();">')
+		
+	});
+	
+});
